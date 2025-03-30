1f93889973dc1cf8b5ddab67bc61d77e791a0f4c
GanttFlow Modular <ganttflowmodular@gmail.com>
2025-03-30 16:45:31 -0600

    Delete front directory

--- front/app/lib/api.ts
+++
@@ -1,8 +0,0 @@
-// Exportar funciones de auth.ts
-export * as auth from './api/auth/auth';
-
-// Exportar funciones de users.ts
-export * as usersAdmin from './api/admin/users';
-
-// Exportar funciones de permissions.ts
-export * as permissionsAdmin from './api/admin/permissions';
