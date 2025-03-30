4e789f4c26f1fbef6bffc0ca4468f7e4daa066fc
GanttFlow Modular <ganttflowmodular@gmail.com>
2025-03-30 16:45:20 -0600

    Delete back directory

--- back/routes/settings.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { updateSettings, getSettings } from '../controllers/settings.controller.js';
-
-const router = express.Router();
-
-router.put('/settings', updateSettings); // Actualizar ajustes
-router.get('/settings', getSettings); // Obtener ajustes
-
-export default router;
